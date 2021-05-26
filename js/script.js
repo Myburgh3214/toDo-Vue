51fc02b8c8c3fb1895eb31c902f82ed829697240
Cornéll Myburgh <cornellmyburgh@outlook.com>
2021-05-26 13:37:36 +0200

    Test 2

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let app = new Vue({
                   },
 	methods: {
         saveTask : function(){
-            const data = JSON.stringify(this.todos)///works
+            const data = JSON.stringify(this.todos)///test
         window.localStorage.setItem('todos', data);
         console.log(JSON.parse(window.localStorage.getItem('todos')))
 
