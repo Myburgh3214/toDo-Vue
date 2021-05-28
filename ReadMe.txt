f36526c8718db029c3d6e4e577d3a04e6d9a19b1
Cornéll Myburgh <cornellmyburgh@outlook.com>
2021-05-28 12:46:49 +0200

    Fixed checkbox bug, but safe function not working properly due to mounted in JS being buggy

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,4 @@
-Atleast 1 field must be filled in order to submit into the list.
-The list is editable and once all your tasks are complete, you can can check the checkbox above it to strikethrough the entire list.
-Use clear all to delete all the list contents.
-Use sort the sort the list contents in alpahbetical order.
-Use sort the list contents in alphabetical order.
+The field must be filled in order to submit into the list.
+The list is editable and once all your tasks are complete, can be edited by simply highlighting the text.
+Once the task has been submitted to the list. You can use the "X" to delete the task and the checkbox to strikethrough them.
+
--- js/script.js
+++ js/script.js
@@ -1,47 +1,41 @@
 let app = new Vue({
-	el: '#app',
-	data: {
-        
-		todoText: '',
-            weekday: new Date().toString().slice(0, 3),
-		todos: [{
-        },	
-		],
+el: '#app',
+data: {
+    todoText: '',
+    weekday: new Date().toString().slice(0, 3),
+    todos: [],
     priorities: [{prio: 2, label: 'priority: low'}, {prio: 1, label: 'priority: medium'}, {prio: 0, label: 'priority: high'}],
-      selectedPriority: {priority: 2, label: 'low'},
+    selectedPriority: {priority: 2, label: 'low'},
 
-                  },
-	methods: {
-        saveTask : function(){
-            const data = JSON.stringify(this.todos)///test
-        window.localStorage.setItem('todos', data);
-        console.log(JSON.parse(window.localStorage.getItem('todos')))
+},
 
-        },
-		addTodo: function() {
-			let newTodo = this.todoText.trim();
-                 if (this.todoText !== '') {
-        this.todos.push({
-          text: this.todoText,
-          wDay: this.weekday,
-        priority: this.selectedPriority.label,
-          done: false
-        }
-			);
-			this.todoText = ''
-                 }
-		},
-        removeItem(todo) {
-      this.todos.splice(todo, 1)
-    },
-        taskClasses(task) {
-      const classes = {
-        done: task.checked,
+methods: {
+    saveTask : function(){
+    const data = JSON.stringify(this.todos)
+    localStorage.setItem('todos', data);
+    JSON.parse(localStorage.getItem("todos"))          
+},
+addTodo: function() {
+    let newTodo = this.todoText.trim();
+    if (this.todoText !== '') {
+    this.todos.push({
+    text: this.todoText,
+    wDay: this.weekday,
+    priority: this.selectedPriority.label,
+    done: false
+});
+    this.todoText = ''
+}
+},
+removeItem(todo) {
+    this.todos.splice(todo, 1)
+},
+taskClasses(task) {
+    const classes = {
         [task.priority.prio]: true,
-      }
-      console.log(classes)
-      return classes;
-    }
-  }
-    });
+}    
+return classes;
+}
+}
+});
 
